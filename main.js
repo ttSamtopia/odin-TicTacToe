32c37fd59fc4fb069460b5aa4a925e25edf999b4
ttSamtopia <s-linden@live.se>
2026-09-22 23:08:05 +0200

    Add method, getCell

--- main.js
+++ main.js
@@ -6,6 +6,17 @@ const Gameboard = (function () {
         ["X", "O", "X"],
     ]
 
+    return {
+
+        getCell (row, column) {
+            row = parseInt(row);
+            column = parseInt(column);
+            if (!(row >= 1 && row <= 3)) throw Error("Incorrect Row Number (1-3)");
+            if (!(column >= 1 && column <= 3)) throw Error("Incorrect Column Number (1-3)");
+            row--;
+            column--;
+            return _gameboard[row][column];
+        },
 console.log(Gameboard.getCell(1,3));
 console.log(Gameboard.getGameboard());
 Gameboard.reset()
