cc45d4b96bdc0f85a5aedddb51943ed3da56e6fd
ttSamtopia <s-linden@live.se>
2026-09-24 17:03:45 +0200

    Fix diagonal win check now returns false boolean

--- main.js
+++ main.js
@@ -118,18 +118,24 @@ const gameFlow = (function () {
                 _checkMatchingMarker(Gameboard.getCell(1,1), Gameboard.getCell(2,2), Gameboard.getCell(3,3)) || _checkMatchingMarker(Gameboard.getCell(1,3), Gameboard.getCell(2,2), Gameboard.getCell(3,1))
             ) {
                 return true;
+            } else {
+                return false;
             }
         } else if (cordsString === "1,1" || cordsString === "3,3") {
             if (
                 _checkMatchingMarker(Gameboard.getCell(1,1), Gameboard.getCell(2,2), Gameboard.getCell(3,3))
             ) {
                 return true;
+            } else {
+                return false;
             }
         } else if (cordsString === "1,3" || cordsString === "3,1") {
             if (
                 _checkMatchingMarker(Gameboard.getCell(1,3), Gameboard.getCell(2,2), Gameboard.getCell(3,1))
             ) {
                 return true;
+            } else {
+                return false;
             }
         } else {
             return false;
