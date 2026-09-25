482d2f82322aaff3f0ff219b6ec76e4370ef4b10
ttSamtopia <s-linden@live.se>
2026-09-25 18:56:24 +0200

    Fix bug where empty squares would be ignored

--- main.js
+++ main.js
@@ -210,7 +210,9 @@ const display = (function () {
                 let row = cell.dataset.row;
                 let column = cell.dataset.column;
                 let cellContent = gameboard.getCell(row, column);
-                if (cellContent === "") { continue; }
+                if (cellContent === "") { 
+                    cell.innerHTML = "";
+                }
                 else if (cellContent === "X") {
                     cell.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x preview-icon"><path d="M22 2 2 22"/><path d="m2 2 20 20"/></svg>`;
                 } else {
